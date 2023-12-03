abbf326cf41b609b1f9c52cf9fe3240f45acf5e0
conko1 <contomartin11@gmail.com>
2023-12-03 11:25:28 +0100

    Implementation of LandingView and creating vuex store template

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import RegistrationView from "@/views/RegistrationView.vue";
 import LoginView from "@/views/LoginView.vue";
+import LandingView from "@/views/LandingView.vue";
 
 const routes = [
   {
+    path: "/",
+    name: "LandingView",
+    component: LandingView
+  },
+  {
     path: "/register",
-    name: "register",
+    name: "RegistrationView",
     component: RegistrationView
   },
   {
     path: "/login",
-    name: "login",
+    name: "LoginView",
     component: LoginView
   },
 ]
---
+++ src/views/LandingView.vue
@@ -0,0 +1,42 @@
+<template>
+  <main>
+    <div class="position-relative overflow-hidden m-md-3 text-center bg-body-tertiary">
+      <div class="col-md-6 p-lg-5 mx-auto my-5">
+        <h1 class="display-3 fw-bold mb-3">Trackujte čas s nami</h1>
+        <h3 class="fw-normal text-muted mb-3">Konečné riešenie všetkých problémov so sledovaním odpracovaného času vo
+          vašej firme</h3>
+      </div>
+      <div class="product-device shadow-sm d-none d-md-block"></div>
+      <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+    </div>
+
+    <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+      <div class="text-bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+        <div class="my-3 py-3">
+          <h2 class="display-5 mb-5">Stále využívate starú papierovú metódu ?</h2>
+          <p class="lead">V dnešnom rýchlom obchodnom prostredí môže byť sledovanie pracovného času zamestnancov
+            náročnou úlohou. Papierové časové hárky, ručné záznamy a neustály strach z nepresností môžu brzdiť rast
+            vášho podnikania. Čas sú peniaze a každá premárnená minúta ovplyvňuje vaše hospodárske výsledky.</p>
+        </div>
+      </div>
+      <div class="bg-body-tertiary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+        <div class="my-3 p-3">
+          <h2 class="display-5 mb-5">Vyskúšajte online riešenie budúcnosti !</h2>
+          <p class="lead"><span class="fw-bold">HourWatch</span> prináša revolučné prostriedky v riadení pracovnej sily. Naša aplikácia je
+            navrhnutá tak, aby zjednodušila sledovanie času, eliminovala chybu a umožnila vášmu tímu sústrediť sa na to,
+            čo je dôležité - na napredovanie vášho podniku.</p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "LandingView",
+}
+</script>
+
+<style scoped>
+
+</style>
